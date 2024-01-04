70689d797f4259d219e2f094d3c8733e2809e4ac
Yalcin Tatar <yalcintatar41@gmail.com>
2024-01-03 20:07:09 -0800

    add error handling for open ai

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -4,6 +4,7 @@ import { MessageArraySchema } from "@/lib/validators/message";
 import { Commerce7API } from "@/lib/commerce7-api";
 import { chatbotPromptv3 } from "@/lib/prompts/chatbot-prompt-v3";
 import { ChatGPTMessage } from "@/types";
+import { NextResponse } from "next/server";
 
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
@@ -14,36 +15,46 @@ const openai = new OpenAI({
 export const runtime = "edge";
 
 export async function POST(req: Request) {
-  const { messages, tenantId, websiteUrl } = await req.json();
-  console.log("This is post,", tenantId, websiteUrl);
-  // const parsedMessages = MessageArraySchema.parse(messages);
-  // const parsedMessages = console.log("Test post", messages, tenantId);
-  // const outboundMessages: ChatGPTMessage[] = parsedMessages.map((message) => {
-  //   return {
-  //     role: message.isUserMessage ? "user" : "system",
-  //     content: message.text,
-  //   };
-  // });
+  try {
+    const { messages, tenantId, websiteUrl } = await req.json();
+    console.log("This is post,", tenantId, websiteUrl);
+    // const parsedMessages = MessageArraySchema.parse(messages);
+    // const parsedMessages = console.log("Test post", messages, tenantId);
+    // const outboundMessages: ChatGPTMessage[] = parsedMessages.map((message) => {
+    //   return {
+    //     role: message.isUserMessage ? "user" : "system",
+    //     content: message.text,
+    //   };
+    // });
 
-  const commerce7Response = await Commerce7API(tenantId);
+    const commerce7Response = await Commerce7API(tenantId);
 
-  const productInfos = await commerce7Response.json();
+    const productInfos = await commerce7Response.json();
 
-  await messages.unshift({
-    role: "assistant",
-    content: chatbotPromptv3(productInfos, websiteUrl),
-  });
-  // Ask OpenAI for a streaming chat completion given the prompt
-  const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo-1106",
-    stream: true,
-    messages,
-    n: 1,
-    temperature: 0.1,
-  });
+    await messages.unshift({
+      role: "assistant",
+      content: chatbotPromptv3(productInfos, websiteUrl),
+    });
+    // Ask OpenAI for a streaming chat completion given the prompt
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo-1106",
+      stream: true,
+      messages,
+      n: 1,
+      temperature: 0.1,
+    });
 
-  // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response);
-  // Respond with the stream
-  return new StreamingTextResponse(stream);
+    // Convert the response into a friendly text-stream
+    const stream = OpenAIStream(response);
+    // Respond with the stream
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    // Check if the error is an APIError
+    if (error instanceof OpenAI.APIError) {
+      const { name, status, headers, message } = error;
+      return NextResponse.json({ name, status, headers, message }, { status });
+    } else {
+      throw error;
+    }
+  }
 }
