eda85c2916e3f942b4d81ddb252f5bee18b4068f
Yalcin Tatar <yalcintatar41@gmail.com>
2024-01-17 23:30:48 -0800

    improve prompting and add add to cart prompt

--- app/api/chat/functions.ts
+++ app/api/chat/functions.ts
@@ -1,4 +1,5 @@
 import { Commerce7API } from "@/lib/commerce7-api";
+import { wineProductData } from "@/lib/prompts/promptFunction/wine-product-data";
 
 export const functions: any[] = [
   {
@@ -12,7 +13,78 @@ export const functions: any[] = [
       },
     },
   },
+  {
+    type: "function",
+    function: {
+      name: "get_wine_product_information",
+      description: "Get list of wine product list",
+      parameters: {
+        type: "object",
+        properties: {},
+      },
+    },
+  },
+  {
+    type: "function",
+    function: {
+      name: "add_product_to_cart",
+      description:
+        "Add wine product to the cart by generating a specific url for the user. The link format will be [Add to Cart](http://example/?addToCart=example_sku_noC&quantity=example_quantity_number",
+      parameters: {
+        type: "object",
+        properties: {},
+      },
+    },
+  },
+
+  // {
+  //   type: "function",
+  //   function: {
+  //     name: "multi_function",
+  //     description: "Call two functions in one call",
+  //     parameters: {
+  //       type: "object",
+  //       properties: {
+  //         get_wine_product_information: {
+  //           name: "get_wine_product_information",
+  //           description: "Get the wine product list",
+  //         },
+  //         add_product_to_cart: {
+  //           name: "add_product_to_cart",
+  //           description:
+  //             "Add wine product to the cart by generating a specific url for the user. The link format will be [Add to Cart](http://example/?addToCart=example_sku_noC&quantity=example_quantity_number",
+  //         },
+  //       },
+  //     },
+  //   },
+  // },
 ];
+
+async function get_wine_product_information(
+  tenantId: string,
+  websiteUrl: string
+) {
+  const products = await Commerce7API(tenantId, "v1/product");
+  // console.log("hello products", products);
+  console.log({ websiteUrl });
+  return wineProductData(products, websiteUrl);
+}
+
+// async function add_product_to_cart(tenantId: string, websiteUrl: string) {
+//   const products = await Commerce7API(tenantId, "v1/product");
+//   let obj = {};
+
+//   // console.log("hello products", products);
+//   console.log({ websiteUrl });
+//   return wineProductData(products, websiteUrl);
+// }
+
+// async function multi_function(tenantId: string, websiteUrl: string) {
+//   const products = await Commerce7API(tenantId, "v1/product");
+//   // console.log("hello products", products);
+//   console.log({ websiteUrl });
+//   return wineProductData(products, websiteUrl);
+// }
 
 async function get_reservations(
   tenantId: string,
@@ -50,6 +122,11 @@ async function get_reservations(
 
 export async function runFunction(name: string, args: any) {
   switch (name) {
+    case "get_wine_product_information":
+      return await get_wine_product_information(
+        args["tenantId"],
+        args["websiteUrl"]
+      );
     case "get_reservations":
       return await get_reservations(
         args["tenantId"],
