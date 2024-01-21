7c08d96133cc920a19e3b3562fbd4981089bce15
Yalcin Tatar <yalcintatar41@gmail.com>
2024-01-20 21:35:22 -0800

    fix caching

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -4,26 +4,15 @@ import { headers } from "next/headers";
 
 export default async function ChatPage() {
   const cookie = await getCookie();
+
   const headersList = headers();
   //get the website url
   const referer = headersList.get("referer");
-  console.log("referer", referer);
 
   const chat = await getChat(cookie?.value);
 
   if (!chat) {
-    return (
-      <Chat
-        initialMessages={[
-          {
-            id: "1",
-            role: "assistant",
-            content: "Welcome to Commerce7 AI Bot!",
-          },
-        ]}
-        websiteUrl={referer}
-      />
-    );
+    return <Chat websiteUrl={referer} />;
   }
   const filterMessages = chat?.messages.filter(
     (message: any) => message.role !== "system"
