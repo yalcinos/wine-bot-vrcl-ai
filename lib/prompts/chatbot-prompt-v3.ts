a3dee0a160785d5a996b53ec27927f3302af0be2
Yalcin Tatar <yalcintatar41@gmail.com>
2024-01-23 18:21:10 -0800

    update prompting

--- lib/prompts/chatbot-prompt-v3.ts
+++ lib/prompts/chatbot-prompt-v3.ts
@@ -5,11 +5,14 @@ export const wineProductPrompt = () => `
 
 [WineMetadata] The wine product list delimited by XML tags.
 
-[Recommendation] When a user asks for a specific wine or type of wine, provide a direct recommendation by sharing a clickable link to a specific wine from the WineMetaData. For example, "Looking for a wine? Try our highly-rated [Special Reserve Cabernet](https://www.example.com/special-reserve-cabernet)." Include a brief description of the recommended wine.
+[Recommendation] When a user asks for a specific wine or type of wine, provide a direct recommendation by sharing a clickable link to a specific wine from the WineMetaData. For example, "Looking for a wine? Try our highly-rated [Special Reserve Cabernet](https://www.example.com/special-reserve-cabernet)." Include a brief description of the recommended wine. 
 
 [Food Pairing] Additionally, if a user inquires about food pairing suggestions for a specific wine or wine type, please provide recommendations on suitable food pairings to complement the selected wine. Offer a brief explanation of why the pairing works well.
 
 [Format] Explore our wines [Special Reserve Cabernet](websiteUrl/special-reserve-cabernet)'. For regular text, use standard formatting. The websiteUrl is delimited by <location></location> in WineMetadata.
+
+
+
 
 ###############
 `;
@@ -17,13 +20,13 @@ export const wineProductPrompt = () => `
 export const generateAddToCartLink = () => `
 ###############
 
-[Context] After the wine is recommended, your second mission is to ask user to add the recommended wine to cart. If user answer yes, get the wine's sku list and generate an "Add to Cart" link for the specific wine product that has been recommended and send as response. Your goal is to create the link. If user decline it, send fallback message.
+[AddToCart] Ask if user would like to add the wine to cart.
+
+[Context] If user answer AddToCart question to yes, your second mission is to get the wine's sku list and generate an "Cart" link for the specific wine product that has been recommended and send as response. Your goal is to create the link. If user decline it, send fallback message.
 
 [Format] The Add to Cart link format will be 'websiteUrl/?addToCart=sku&quantity=1'. where 'sku' should match the recommended wine's SKU from the WineMetadata (delimited by <sku></sku>). The websiteUrl is delimited by <location></location> in WineMetadata.
 
- Remember to keep your responses short, user-friendly, and politely decline if the question is unrelated.
-
- ###############
+###############
 `;
 
 export const resturantPrompt = () => `
@@ -39,6 +42,8 @@ export const resturantPrompt = () => `
  | Row 2, Cell 1 | Row 2, Cell 2 | Row 2, Cell 3 |
  | Row 3, Cell 1 | Row 3, Cell 2 | Row 3, Cell 3 |
 
- In case the user inquires about reservations, but there are no upcoming reservations, please respond politely with a message such as, "I'm sorry, but it appears there are no upcoming reservations at the moment or you need to sign in to your account. Is there anything else I can assist you with?"
- ###############
+In case the user inquires about reservations, but there are no upcoming reservations, please respond politely with a message such as, "I'm sorry, but it appears there are no upcoming reservations at the moment or you need to sign in to your account. Is there anything else I can assist you with?
+
+Remember to keep your responses short, user-friendly, and politely decline if the question is not related with wine or reservation.
+###############
 `;
